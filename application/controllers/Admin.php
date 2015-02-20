b52a0bc47da086181b28e7a4a8b681988c81f332
TheRocktopus <matthewdbanman@gmail.com>
2015-02-20 09:27:27 -0800

    Completed lab 5 part b
    Signed-off-by: TheRocktopus <matthewdbanman@gmail.com>

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -37,14 +37,74 @@ class Admin extends Application {
 		$this->present($quote);
 	}
 	
+	function confirm()
+	{
+		$record = $this->quotes->create();
+		
+		// assign form fields
+		$id = $this->input->post('id');
+		$record->who = $this->input->post('who');
+		$record->mug = $this->input->post('mug');
+		$record->what = $this->input->post('what');
+		
+		// validate field content
+		if( empty($record->who) )
+		{
+			$this->errors[] = 'You must specify an author';
+		}
+		if( strlen($record->what) < 20 )
+		{
+			$this->errors[] = 'A quotation must be at least 20 characters long.';
+		}
+		
+		// redisplay page if there are errors
+		if( count($this->errors) > 0)
+		{
+			$this->present($record);
+			return;
+		}
+		
+		// save the record
+		if( empty($id) )
+		{
+			// initialize id and vote fields
+			$record->id = $this->quotes->highest() + 1;
+			$record->vote_total = 0;
+			$record->vote_count = 0;
+			
+			// add new quote
+			$this->quotes->add($record);
+			
+		}
+		else
+		{
+			// update quote
+			$record->id = $id;
+			$this->quotes->update($record);
+		}
+		
+		redirect('/admin');
+	}
+	
 	// present a quote
 	function present($quote)
 	{
+		// error formatting
+		$message = '';
+		if( count($this->errors) > 0 )
+		{
+			foreach( $this->errors as $error )
+			{
+				$message .= $error . BR;
+			}
+		}
+		
 		// make quote form fields
-		$this->data['fid'] = makeTextField('ID#', 'id', $quote->id);
+		$this->data['fid'] = makeTextField('ID#', 'id', $quote->id, "Unique quote identifier, system-assigned",10,10,true);
 		$this->data['fwho'] = makeTextField('Author', 'who', $quote->who);
 		$this->data['fmug'] = makeTextField('Picture', 'mug', $quote->mug);
 		$this->data['fwhat'] = makeTextField('The Quote', 'what', $quote->what);
+		$this->data['message'] = $message;
 		
 		// form submit button
 		$this->data['fsubmit'] = makeSubmitButton('Process Quote', 
